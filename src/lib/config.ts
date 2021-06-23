7168da90d315db11f6008cfb177b354f52190053
nixer89 <pbnixer@gmail.com>
2021-06-23 13:24:55 +0200

    test

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -8,8 +8,8 @@ import { XrplNetwork } from 'xpring-js'
 
 // Web gRPC rippleD node endpoints hosted by RippleX
 export enum WebGrpcEndpoint {
-  Main = 'https://xrplcluster.com',
-  Test = 'https://s.altnet.rippletest.net:50051',
+  Main = 'https://testnet.xrpl-labs.com',
+  Test = 'https://testnet.xrpl-labs.com:50051',
 }
 
 // Retry limit for reliable send
@@ -18,5 +18,5 @@ export const retryLimit = 10
 export const INPUT_CSV_FILE = process.env.INPUT_CSV_FILE || './input.csv';
 export const OUTPUT_CSV_FILE = process.env.OUTPUT_CSV_FILE || './output.csv';
 export const XRPL_NETWORK = process.env.XRPL_NETWORK === 'mainnet' ? XrplNetwork.Main : XrplNetwork.Test || XrplNetwork.Test;
-export const GPRC_URL = process.env.GPRC_URL || 'https://s.altnet.rippletest.net:50051';
+export const GPRC_URL = process.env.GPRC_URL || 'https://testnet.xrpl-labs.com:50051';
 export const XRPL_SECRET = process.env.XRPL_SECRET || 'shTAjRHoxanFFx6TiPKEVJYVeXRqj';
