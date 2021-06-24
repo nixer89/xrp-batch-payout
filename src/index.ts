d826abc70f90407e9c3718cbf27418bb9d575484
nixer89 <pbnixer@gmail.com>
2021-06-24 09:17:29 +0200

    do not listen on file add

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,6 @@ function watchInputFile() {
 
     console.log("watching for file changes on " + config.INPUT_CSV_FILE);
 
-    watcher.on('add', () => {
-        console.log("received file change, will execute payout");
-        payout();
-    })
-
     watcher.on('change', () => {
         console.log("received file change, will execute payout");
         payout();
