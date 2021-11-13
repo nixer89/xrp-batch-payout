111bc155976ede4184c86e0a37ff927a85cceba0
nixer89 <pbnixer@gmail.com>
2021-11-13 13:04:37 +0100

    v.2.0.1: do not start the program when it is already running

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,25 @@ import payout from './apps/payout';
 import * as config from './lib/config';
 import * as chokidar from 'chokidar'
 
+let isRunning:boolean = false;
+
 function watchInputFile() {
     let watcher = chokidar.watch(config.INPUT_CSV_FILE);
 
     console.log("watching for file changes on " + config.INPUT_CSV_FILE);
 
-    watcher.on('change', () => {
-        console.log("received file change, will execute payout");
-        payout();
+    watcher.on('change', async () => {
+        if(!isRunning) {
+            isRunning = true;
+
+            console.log("received file change, will execute payout");
+            await payout();
+            
+            isRunning = false;
+        } else {
+            console.log("received file change but not starting script because it is running already!")
+        }
+        
     })
 }
 
